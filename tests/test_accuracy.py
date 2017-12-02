7532edcf93db38a83817ff0579fd45caa99c35a0
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2017-12-02 09:26:18 +0000

    new performance metric

--- tests/test_accuracy.py
+++ tests/test_accuracy.py
@@ -10,9 +10,15 @@ import pandas as pd
 import subprocess
 import unittest
 
-from fuzzymatcher import link_table
+from fuzzymatcher import link_table, fuzzy_left_join
 from fuzzymatcher.data_getter_cartesian import DataGetterCartesian
 from fuzzymatcher.matcher import Matcher
+
+def get_commit_hash():
+    process = subprocess.Popen("git describe --always", stdout=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    commit_hash = output.decode("utf-8").replace("\n", "")
+    return commit_hash
 
 def link_table_percentage_correct(link_table):
     lt = link_table.copy()
@@ -29,35 +35,54 @@ class DatagetterAccuracy(unittest.TestCase):
     They're not pass fail but they log how well the matcher is doing
     """
 
-    dg = DataGetterCartesian()
-    m = Matcher(data_getter = dg)
+    def test_data_100(self):
+        dg = DataGetterCartesian()
+        m = Matcher(data_getter = dg)
 
-    df_left = pd.read_csv("tests/data/left_3.csv")
-    df_right = pd.read_csv("tests/data/right_3.csv")
+        df_left = pd.read_csv("tests/data/left_3.csv")
+        df_right = pd.read_csv("tests/data/right_3.csv")
 
-    on = ["first_name", "surname", "dob", "city"]
+        on = ["first_name", "surname", "dob", "city"]
 
-    m.add_data(df_left, df_right, on, on)
+        m.add_data(df_left, df_right, on, on)
 
-    m.match_all()
-    lt = m.get_formatted_link_table()
+        m.match_all()
+        lt = m.get_formatted_link_table()
 
-    cartesian_perc = link_table_percentage_correct(lt)
+        cartesian_perc = link_table_percentage_correct(lt)
 
+        lt2 = link_table(df_left, df_right, on, on)
+        sqlite_perc = link_table_percentage_correct(lt2)
 
-    lt2 = link_table(df_left, df_right, on, on)
-    sqlite_perc = link_table_percentage_correct(lt2)
+        this_record = {}
+        this_record["datetime"] = datetime.datetime.now().isoformat()
+        this_record["commit_hash"] = get_commit_hash()
+        this_record["datagetter_cartesian"] = cartesian_perc
+        this_record["datagetter_sqlite"] = sqlite_perc
+        this_record["test_type"] = "left_3"
 
-    process = subprocess.Popen("git describe --always", stdout=subprocess.PIPE, shell=True)
-    output, error = process.communicate()
-    commit_hash = output.decode("utf-8").replace("\n", "")
+        with open("tests/datagetter_performance.txt", "a") as myfile:
+            myfile.writelines(json.dumps(this_record) + "\n")
 
+    def test_la_data(self):
+        ons = pd.read_csv("tests/data/las_ons.csv")
+        os = pd.read_csv("tests/data/las_os.csv")
 
-    this_record = {}
-    this_record["datetime"] = datetime.datetime.now().isoformat()
-    this_record["commit_hash"] = commit_hash
-    this_record["datagetter_cartesian"] = cartesian_perc
-    this_record["datagetter_sqlite"] = sqlite_perc
+        df_joined = fuzzy_left_join(ons, os, left_on = ["lad16nm"], right_on = ["name"])
 
-    with open("tests/datagetter_performance.txt", "a") as myfile:
-        myfile.writelines(json.dumps(this_record) + "\n")
+        rename = {"lad16cd": "ons_code", "code": "os_code", "lad16nm": "ons_name", "name": "os_name"}
+        df_joined = df_joined.rename(columns=rename)
+        col_order = ["best_match_score", "ons_name", "os_name", "ons_code", "os_code"]
+
+        num_records = len(df_joined)
+        correct_binary = (df_joined["ons_code"] == df_joined["os_code"])
+        perc_correct = correct_binary.sum()/num_records
+
+        this_record = {}
+        this_record["datetime"] = datetime.datetime.now().isoformat()
+        this_record["commit_hash"] = get_commit_hash()
+        this_record["perc_correct"] = perc_correct
+        this_record["test_type"] = "local_authority"
+
+        with open("tests/realexample_performance.txt", "a") as myfile:
+            myfile.writelines(json.dumps(this_record) + "\n")
